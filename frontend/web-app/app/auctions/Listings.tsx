8e2735c17ba4816a85027ab514335e844fd7740a
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2023-12-09 15:18:27 +0100

    Added types to the project

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import AuctionCard from './AuctionCard';
+import React from "react";
+import AuctionCard from "./AuctionCard";
+import { Auction, PagedResult } from "@/types";
 
-async function getData() {
-    const res = await fetch('http://localhost:6001/search?pageSize=10');
+async function getData(): Promise<PagedResult<Auction>> {
+  const res = await fetch("http://localhost:6001/search?pageSize=10");
 
-    if (!res.ok) throw new Error("Failed to fetch data");
+  if (!res.ok) throw new Error("Failed to fetch data");
 
-    return res.json();
+  return res.json();
 }
 
 export default async function Listings() {
-    const data = await getData();
+  const data = await getData();
 
-    return (
-        <div className='grid grid-cols-4 gap-6'>
-            {data && data.results.map((auction: any) => (
-                <AuctionCard auction={auction} key={auction.id} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      {data &&
+        data.results.map((auction) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))}
+    </div>
+  );
 }
