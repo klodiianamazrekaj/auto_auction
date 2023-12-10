4869c9446bfeee42302d3a7c5b23562ce17bb29b
klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>
2023-12-11 00:42:31 +0100

    Added the search functionality

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,22 +1,22 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 
 //icons
 import { RiAuctionLine } from "react-icons/ri";
+import Search from "./Search";
+import Logo from "./Logo";
 
 export default function Navbar() {
-    console.log('Client component')
-    return (
-        <header 
-            className='sticky top-0 z-50 flex justify-between 
-                bg-white p-5 items-center text-gray-800 shadow-md'>
-            <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-                <RiAuctionLine size={34}/>
-                <div>Auto Auction</div>
-            </div>
-            <div>Search</div>
-            <div>Login</div>
-        </header>
-    )
+  console.log("Client component");
+  return (
+    <header
+      className="sticky top-0 z-50 flex justify-between 
+                bg-white p-5 items-center text-gray-800 shadow-md"
+    >
+      <Logo />
+      <Search />
+      <div>Login</div>
+    </header>
+  );
 }
